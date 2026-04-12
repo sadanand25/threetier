cf3050280cb21559e0384c15e8d12fa03b92826a
sadanand25 <35913596+sadanand25@users.noreply.github.com>
2026-04-12 20:48:59 +0100

    Update index.js

--- app/index.js
+++ app/index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.json({ message: 'CI/CD Demo - Deployed via GitHub Actions!', timestamp: new Date() });
+  res.json({ message: 'CI/CD  - Deployed via GitHub Actions!', timestamp: new Date() });
 });
 
 app.get('/health', (req, res) => {
